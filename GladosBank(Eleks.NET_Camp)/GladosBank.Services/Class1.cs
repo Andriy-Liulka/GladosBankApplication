60cf7ca642a44ca03462c9937d5bbc6028de44b9
Andriy-Liulka <87704748+Andriy-Liulka@users.noreply.github.com>
2021-10-13 19:32:48 +0300

    no message

--- GladosBank(Eleks.NET_Camp)/GladosBank.Services/Class1.cs
+++ GladosBank(Eleks.NET_Camp)/GladosBank.Services/Class1.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace GladosBank.Services
 {
-    public class Class1
+    class Class1
     {
-
     }
 }
