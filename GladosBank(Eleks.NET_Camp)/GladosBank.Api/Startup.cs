46c6ccea1331021947a71d09aad5c22b885b76c3
Andriy-Liulka <87704748+Andriy-Liulka@users.noreply.github.com>
2021-10-14 22:27:06 +0300

    Added DBContext to ConfigureServices

--- GladosBank(Eleks.NET_Camp)/GladosBank.Api/Startup.cs
+++ GladosBank(Eleks.NET_Camp)/GladosBank.Api/Startup.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GladosBank.Domain;
 
 namespace GladosBank.Api
 {
@@ -23,6 +24,7 @@ namespace GladosBank.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationContext>();
             services.AddRazorPages();
         }
 
