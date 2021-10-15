b3bbeefa31b01969b1228f863eb21a6bf2091bac
Andriy-Liulka <87704748+Andriy-Liulka@users.noreply.github.com>
2021-10-16 00:47:15 +0300

    Added migrations.

---
+++ GladosBank(Eleks.NET_Camp)/GladosBank.Domain/Migrations/20211015214543_FirstMigration.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GladosBank.Domain.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Currency",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Symbol = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Currency", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Documentations",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    DescriptionDocumentation = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Documentations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Informations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DescriptionInformation = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Informations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Discriminator = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    _AccountId = table.Column<int>(type: "int", nullable: true),
+                    Salary = table.Column<decimal>(type: "decimal(18,2)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    _UserId = table.Column<int>(type: "int", nullable: true),
+                    _CurrencyId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Notes = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Accounts_Currency__CurrencyId",
+                        column: x => x._CurrencyId,
+                        principalTable: "Currency",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Accounts_Users__UserId",
+                        column: x => x._UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts__CurrencyId",
+                table: "Accounts",
+                column: "_CurrencyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts__UserId",
+                table: "Accounts",
+                column: "_UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users__AccountId",
+                table: "Users",
+                column: "_AccountId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Accounts__AccountId",
+                table: "Users",
+                column: "_AccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Accounts_Currency__CurrencyId",
+                table: "Accounts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Accounts_Users__UserId",
+                table: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "Documentations");
+
+            migrationBuilder.DropTable(
+                name: "Informations");
+
+            migrationBuilder.DropTable(
+                name: "Currency");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Accounts");
+        }
+    }
+}
--- GladosBank(Eleks.NET_Camp)/GladosBank.Domain/Models/ApplicationContext.cs
+++ GladosBank(Eleks.NET_Camp)/GladosBank.Domain/Models/ApplicationContext.cs
@@ -18,11 +18,9 @@ namespace GladosBank.Domain
         DbSet<Information> Informations { get; set; }
         DbSet<User> Users { get; set; }
         DbSet<Worker> Workers { get; set; }
-        
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(@"Server=ANDRIJ-PC\SQLSERVER2021;Database=EleksDOTNETCamp2021DataBase_GladosBank;User Id=andriy;Password=Fylhsq;  Trusted_Connection=False;");
-        }
 
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+        }
     }
 }
