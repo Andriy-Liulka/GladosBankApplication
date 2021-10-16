d284e4baf03b4c58a19c1b57a83724f8be179d1d
Andriy-Liulka <87704748+Andriy-Liulka@users.noreply.github.com>
2021-10-16 20:39:13 +0300

    Little changes to find problem with possibility of creating migrations.

--- GladosBank(Eleks.NET_Camp)/GladosBank.Api/Startup.cs
+++ GladosBank(Eleks.NET_Camp)/GladosBank.Api/Startup.cs
@@ -32,6 +32,7 @@ namespace GladosBank.Api
                 option.UseSqlServer(cs);
             });
 
+;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
