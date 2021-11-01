5c44e2cfe0f876b207af32b2e162e28fc2f549a2
Andriy-Liulka <andriy0liulka@gmail.com>
2021-11-01 23:52:08 +0200

    Added UpdateControllerArgs for UndateController.

---
+++ GladosBank(Eleks.NET_Camp)/GladosBank.Api/Models/Args/UserControllerArgs/UpdateUserArgs.cs
@@ -0,0 +1,14 @@
+﻿using GladosBank.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GladosBank.Api.Models.Args.UserControllerArgs
+{
+    public class UpdateUserArgs
+    {
+        public int Id { get; set; }
+        public User NewUser { get; set; }
+    }
+}
