56fd83ad72e6130679c108a0a4d0a7b26fac0196
Andriy-Liulka <andriy0liulka@gmail.com>
2021-11-07 20:56:50 +0200

    Added new exception.

---
+++ GladosBank(Eleks.NET_Camp)/GladosBank.Services/Exceptions/InvalidUserLoginException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GladosBank.Services.Exceptions
+{
+    public class InvalidUserLoginException : Exception
+    {
+        public InvalidUserLoginException(string Login) : base($"You entered Login-> \"{Login}\" that doesn't exist of in database !") { }
+    }
+}
