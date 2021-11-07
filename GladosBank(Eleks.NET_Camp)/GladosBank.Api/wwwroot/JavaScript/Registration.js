34a0275a9336549f4500bb4bde515c4a3527c14c
Andriy-Liulka <andriy0liulka@gmail.com>
2021-11-07 20:56:24 +0200

    Added static files.

---
+++ GladosBank(Eleks.NET_Camp)/GladosBank.Api/wwwroot/JavaScript/Registration.js
@@ -0,0 +1,11 @@
+﻿function SubmitClick() {
+    var phone = document.getElementById("Phone").value;
+    var email = document.getElementById("Email").value;
+    var login = document.getElementById("Login").value;
+    var password = document.getElementById("Login").value;
+    var role = document.getElementById("Role").value;
+    //let Alert = new CustomAlert(phone + email + login + password + role);
+    //window.Alert = Alert;
+    
+
+}
---
+++ GladosBank(Eleks.NET_Camp)/GladosBank.Api/wwwroot/JavaScript/SignIn.js
@@ -0,0 +1,6 @@
+function SignInClick() {
+  var login = document.getElementById("Login").value;
+  var password = document.getElementById("Password").value;
+    alert(login + password);
+
+}
