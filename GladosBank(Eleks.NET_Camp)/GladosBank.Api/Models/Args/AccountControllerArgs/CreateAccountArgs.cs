c095052366b4452f336311ed07d6b4aacb7ee467
Andriy-Liulka <andriy0liulka@gmail.com>
2021-11-15 20:16:06 +0200

    Added classes for account managing.

---
+++ GladosBank(Eleks.NET_Camp)/GladosBank.Api/Models/Args/AccountControllerArgs/CreateAccountArgs.cs
@@ -0,0 +1,17 @@
+﻿using GladosBank.Api.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GladosBank.Api.Models.Args.AccountControllerArgs
+{
+    public sealed class CreateAccountArgs
+    {
+        [Required]
+        public AccountDto Account { get; set; }
+        [Required]
+        public int CustomerId { get; set; }
+    }
+}
---
+++ GladosBank(Eleks.NET_Camp)/GladosBank.Api/Models/DTO/AccountDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GladosBank.Api.Models.DTO
+{
+    public sealed class AccountDto
+    {
+        public string CurrencyCode { get; set; }
+        public decimal Amount { get; set; }
+        public string Notes { get; set; }
+        public DateTime DateOfCreating { get; set; }
+    }
+}
