56a3a5dc9b4572fb3f44daba05ab2dc9d2a99f5d
Andriy-Liulka <andriy0liulka@gmail.com>
2021-11-15 20:14:39 +0200

    Made fields required.

--- GladosBank(Eleks.NET_Camp)/GladosBank.Api/Models/Args/UserControllerArgs/CreateUserArgs.cs
+++ GladosBank(Eleks.NET_Camp)/GladosBank.Api/Models/Args/UserControllerArgs/CreateUserArgs.cs
@@ -1,6 +1,7 @@
 ﻿using GladosBank.Domain.Models_DTO;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,9 @@ namespace GladosBank.Api.Models.Args.UserControllerArgs
 {
     public sealed class CreateUserArgs
     {
-        public UserDTO MyUser { get; set; }
+        [Required]
+        public UserDto MyUser { get; set; }
+        [Required]
         public string Role { get; set; }
     }
 }
