21a027b3268d07c8b82d6b0d239cd2aea5aaa05b
Andriy-Liulka <andriy0liulka@gmail.com>
2021-11-19 01:21:19 +0200

    Added two new models.

---
+++ GladosBank(Eleks.NET_Camp)/GladosBank.Api/Models/Args/AccountControllerArgs/DeleteAccountArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GladosBank.Api.Models.Args.AccountControllerArgs
+{
+    public class DeleteAccountArgs
+    {
+        [Required]
+        public int Id { get; set; }
+    }
+}
