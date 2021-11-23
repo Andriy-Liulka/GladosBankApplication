f1efddfb7b3dfb85b816de3139895a8ffa5b454d
Andriy-Liulka <andriy0liulka@gmail.com>
2021-11-23 16:03:30 +0200

    Added new field "role" to UpdateUserArgs.

--- GladosBank(Eleks.NET_Camp)/GladosBank.Api/Models/Args/UserControllerArgs/UpdateUserArgs.cs
+++ GladosBank(Eleks.NET_Camp)/GladosBank.Api/Models/Args/UserControllerArgs/UpdateUserArgs.cs
@@ -11,5 +11,6 @@ namespace GladosBank.Api.Models.Args.UserControllerArgs
         public string Login { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
+        public string Role { get; set; }
     }
 }
