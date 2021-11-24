c7188e17418463b4870d2fbba296b105a5351ee1
Andriy-Liulka <andriy0liulka@gmail.com>
2021-11-24 11:08:15 +0200

    Added admin pages.

---
+++ GladosBank(Eleks.NET_Camp)/GladosBank.Api/wwwroot/Accounts/Admin/JavaScript/CheckWhetherAdmin.js
@@ -0,0 +1,19 @@
+﻿
+var token = localStorage.getItem('jwtToken');
+axios.get("https://localhost:5001/api/User/GetUserData", {
+    headers: { Authorization: `Bearer ${token}` }
+})
+    .then((responce) => {
+        var getResponse = responce.status;
+        if (getResponse == 200 && responce.data.role =="Admin") {
+            console.log("Authorized successfully");
+        }
+        else {
+            console.log(getResponse);
+            window.location = "../../../html/SignIn.html";
+        }
+
+    }).catch((error) => {
+        console.log(error);
+        window.location = "../../../html/SignIn.html";
+    });
