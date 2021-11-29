ff666255cdc408026ba232d086a370d2337532d1
Andriy-Liulka <andriy0liulka@gmail.com>
2021-11-29 14:12:33 +0200

    Added checking whether this page is customers page.

---
+++ GladosBank(Eleks.NET_Camp)/GladosBank.Api/wwwroot/Accounts/Worker/JavaScript/CheckWhetherWorker.js
@@ -0,0 +1,20 @@
+﻿
+var token = localStorage.getItem('jwtToken');
+axios.get("https://localhost:5001/api/User/GetUserData", {
+    headers: { Authorization: `Bearer ${token}` }
+})
+    .then((responce) => {
+        var getResponse = responce.status;
+        if (getResponse == 200 && responce.data.role == "Worker") {
+            console.log("Authorized successfully");
+        }
+        else {
+            console.log(getResponse);
+            SignOut();
+        }
+
+    }).catch((error) => {
+        console.log(error);
+        window.location = "../../../html/SignIn.html";
+    });
+
