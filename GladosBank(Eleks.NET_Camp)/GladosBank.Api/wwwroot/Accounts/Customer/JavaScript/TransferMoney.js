6ada4f31b770b7bb6e4811b3cd66dd993b316934
Andriy-Liulka <andriy0liulka@gmail.com>
2021-12-01 22:06:11 +0200

    Corrected gramatical mistakes with "distination" to "destination".

--- GladosBank(Eleks.NET_Camp)/GladosBank.Api/wwwroot/Accounts/Customer/JavaScript/TransferMoney.js
+++ GladosBank(Eleks.NET_Camp)/GladosBank.Api/wwwroot/Accounts/Customer/JavaScript/TransferMoney.js
@@ -10,11 +10,14 @@ function TransferMoneyClick(id) {
 function SendClick() {
 
     var amount = document.getElementById("Amount").value;
-    var distantLogin = document.getElementById("Distant").value;
-    if (amount == "" || distantLogin == "" || distantLogin == null || amount == null) {
+    var destinationLogin = document.getElementById("Distant").value;
+    if (amount == "" || destinationLogin == "" || destinationLogin == null || amount == null) {
         alert("Fields must be filled !");
         return false;
     }
+
+    localStorage.setItem("DestinationLogin", destinationLogin);
+
 
     var sourceId = localStorage.getItem("SourceAccountId");
 
@@ -29,7 +32,7 @@ function SendClick() {
                 var currency = responce.data;
 
                 localStorage.setItem("AmountToTransfer", amount);
-                localStorage.setItem("DistantLogin", distantLogin);
+                localStorage.setItem("DestinationLogin", destinationLogin);
                 localStorage.setItem("TransferCurrency", currency);
 
                 RedirectTransferPageChooseDistinationPage();
@@ -42,7 +45,7 @@ function SendClick() {
 
 function TransferMoneyGetAllPossibleDistinationAccounts() {
     var currency = localStorage.getItem("TransferCurrency");
-    var destinationLogin = localStorage.getItem("DistantLogin");
+    var destinationLogin = localStorage.getItem("DestinationLogin");
     axios.get("https://localhost:5001/api/Account/GetAccountsForCurrencyCode",
         {
             params:
@@ -58,11 +61,11 @@ function TransferMoneyGetAllPossibleDistinationAccounts() {
             console.log(responce.data);
             var accounts = responce.data;
             console.log(accounts);
-            var distantAccountLogin = localStorage.getItem("DistantLogin");
+            var destinationAccountLogin = localStorage.getItem("DestinationLogin");
             for (var i = 0; i < accounts.length; i++) {
-                var distandAccountId = accounts[i].id;
-                document.getElementById("board").innerHTML += `<div onclick="TransactMoney(this.id)" id=${distandAccountId} style = "margin: 10px; color:black;background-color: red; width: 400px; min-height: 80px;" >` +
-                    `<p>Account wwner -> ${distantAccountLogin}</p>`+
+                var destinationAccountId = accounts[i].id;
+                document.getElementById("board").innerHTML += `<div onclick="TransactMoney(this.id)" id=${destinationAccountId} style = "margin: 10px; color:black;background-color: red; width: 400px; min-height: 80px;" >` +
+                    `<p>Account wwner -> ${destinationAccountLogin}</p>`+
                     `<p>Number-> ${i + 1}</p>` +
                     `<p>Currency-> ${accounts[i].currencyCode} ` +
                     `</div>`;
