0a81a62cad2159c7e693024c41b0af3fc5d1fc4a
Andriy-Liulka <andriy0liulka@gmail.com>
2021-12-05 01:45:01 +0200

    Additional changing to previos one.

--- GladosBank(Eleks.NET_Camp)/GladosBank.Api/wwwroot/Accounts/General/JavaScript/Settings.js
+++ GladosBank(Eleks.NET_Camp)/GladosBank.Api/wwwroot/Accounts/General/JavaScript/Settings.js
@@ -1,5 +1,6 @@
-﻿localStorage.setItem("PaginationPageSize", 6);
+﻿
 localStorage.setItem("PaginationPageIndex", 0)
+localStorage.setItem("PaginationPageSize", 6);
 
 localStorage.setItem("PaginationTransferHistoryPageIndex", 0)
 localStorage.setItem("PaginationTransferHistoryPageSize", 10)
