809ea34c6b4331b06e806c1c0ab17755839a9c46
Andriy-Liulka <andriy0liulka@gmail.com>
2021-12-10 23:19:33 +0200

    Added role enum.

---
+++ GladosBank(Eleks.NET_Camp)/GladosBank.Domain/Models/Enums/RolesEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GladosBank.Domain.Models.Enums
+{
+    public static class RolesEnum
+    {
+        public static string Customer = "Customer";
+        public static string Admin = "Admin";
+        public static string Worker = "Worker";
+    }
+}
