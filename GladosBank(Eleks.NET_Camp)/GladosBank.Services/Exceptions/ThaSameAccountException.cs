96640c31b91d1397ee3f4e0ddd89860a2a5e5244
Andriy-Liulka <andriy0liulka@gmail.com>
2021-12-10 14:12:58 +0200

    Added new exception to check whether source and distination are the same.

---
+++ GladosBank(Eleks.NET_Camp)/GladosBank.Services/Exceptions/ThaSameAccountException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GladosBank.Services.Exceptions
+{
+    public class ThaSameAccountException : BusinessLogicException
+    {
+        public ThaSameAccountException() : base($"SourceAccount and DistinationAccount are the same !") { }
+    }
+}
