0940b56497a263f5a5227791bc179f9d29bf3bb4
Andriy-Liulka <andriy0liulka@gmail.com>
2021-12-10 23:53:44 +0200

    Added new exception.

---
+++ GladosBank(Eleks.NET_Camp)/GladosBank.Services/Exceptions/SmallAmountException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GladosBank.Services.Exceptions
+{
+    class SmallAmountException : BusinessLogicException
+    {
+        public SmallAmountException(string amount):base($"You cannot do any operations with \"{amount}\" amount") { }
+    }
+}
