d8932a6a9750289b3391e4e57c4d13c80e09b679
Andriy-Liulka <andriy0liulka@gmail.com>
2021-12-20 17:16:13 +0200

    Added ConvertMoneyArgs.

---
+++ GladosBank(Eleks.NET_Camp)/GladosBank.Api/Models/Args/AccountControllerArgs/ConvertMoneyArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GladosBank.Api.Models.Args.AccountControllerArgs
+{
+    public class ConvertMoneyArgs
+    {
+        [Required]
+        public string SourceCurrency { get; set; }
+        [Required]
+        public string DestinationCurrency { get; set; }
+        [Required]
+        public decimal Amount { get; set; }
+    }
+}
